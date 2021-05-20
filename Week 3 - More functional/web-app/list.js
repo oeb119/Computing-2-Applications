e520b005e194576458ed2d34932e2dde3ef9a12f
oeb119 <73142410+oeb119@users.noreply.github.com>
2021-05-20 13:14:21 +0100

    go

--- Week 3 - More functional/web-app/list.js
+++ Week 3 - More functional/web-app/list.js
@@ -1,5 +1,26 @@
 const List = Object.create(null);
 
+List.sequence = (number) => new Array(number-1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const new_array = Array;
 List.sequence = (n) => new_array(n).fill().map((ignore, k) => k);
 
